58665feca4ec91e6f71b67ad3953dff1f53912a6
Egit777 <jvbrsilva@gmail.com>
2022-06-22 23:50:13 -0300

    navegação

--- lib/telaCalculo.dart
+++ lib/telaCalculo.dart
@@ -1,7 +1,8 @@
 import 'dart:developer';
+import 'main.dart';
 import 'package:flutter/material.dart';
 import 'buttonDropCalculo.dart';
-import 'tabelaDoCalculo.dart';
+import 'graficoo.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -12,7 +13,6 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: TelaCalculo(),
     );
   }
 }
@@ -42,61 +42,67 @@ class _TelaCalculoState extends State<TelaCalculo> {
           ),
           IconButton(
             icon: const Icon(Icons.home),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (context) => MyWidget()));
+            },
           ),
         ],
       ),
       body: ListView(children: [
         Column(children: [
           Padding(
+            child: Container(
+              color: Colors.blue,
+              child:
+                  Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+                const SizedBox(
+                  height: 20,
+                ),
+                TextField(
+                  textAlign: TextAlign.center,
+                  controller: _controllerDias,
+                  keyboardType: const TextInputType.numberWithOptions(),
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: ('DATE:'),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  controller: _controllerLitros,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Litros:',
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextField(
+                  controller: _controllerKM,
+                  keyboardType: const TextInputType.numberWithOptions(),
+                  decoration: const InputDecoration(
+                    hintText: 'Escreva aqui!',
+                    border: OutlineInputBorder(),
+                    labelText: ('KM:'),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextField(
+                  controller: _controllerValor,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Valor/Litros:',
+                  ),
+                ),
+              ]),
+            ),
             padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-              const SizedBox(
-                height: 20,
-              ),
-              TextField(
-                textAlign: TextAlign.center,
-                controller: _controllerDias,
-                keyboardType: const TextInputType.numberWithOptions(),
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: ('Dias:'),
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              TextField(
-                controller: _controllerLitros,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Litros:',
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextField(
-                controller: _controllerKM,
-                keyboardType: const TextInputType.numberWithOptions(),
-                decoration: const InputDecoration(
-                  hintText: 'Escreva aqui!',
-                  border: OutlineInputBorder(),
-                  labelText: ('KM:'),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextField(
-                controller: _controllerValor,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Valor/Litros:',
-                ),
-              ),
-            ]),
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
@@ -138,7 +144,7 @@ class _TelaCalculoState extends State<TelaCalculo> {
                         log(_controllerLitros.text);
 
                         Navigator.of(context).pushReplacement(MaterialPageRoute(
-                            builder: (context) => PageTabelaCalculo()));
+                            builder: (context) => MyWidget()));
                       },
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
@@ -160,7 +166,7 @@ class _TelaCalculoState extends State<TelaCalculo> {
                       ),
                       onPressed: () {
                         Navigator.of(context).pushReplacement(MaterialPageRoute(
-                            builder: (context) => PageTabelaCalculo()));
+                            builder: (context) => MyWidget()));
                       },
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
